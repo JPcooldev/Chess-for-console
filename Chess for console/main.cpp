c83142a8cfa8609fb34dcc7d607fd172fe8491c2
JP <prokopkuba8@gmail.com>
2022-04-02 20:08:08 +0200

    Initial Commit

---
+++ Chess for console/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chess for console
+//
+//  Created by Jakub Prokop on 02.04.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
